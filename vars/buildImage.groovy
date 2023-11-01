7d54d599e683da1aad67330d6c0917cbefe5b584
saidi abdulramoni <144086740+sabdulramoni2@users.noreply.github.com>
2023-11-01 13:28:21 -0400

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'docker build -t ashiwaju/jupiter12:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push nanatwn/demo-app:jma-2.0'
     }
